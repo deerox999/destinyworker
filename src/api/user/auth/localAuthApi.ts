ecdb0699680f11db5d253acd8151ec584e016b8c
jsw <deerox999@gmail.com>
2025-08-03 23:24:04 +0900

    q

--- src/api/user/auth/localAuthApi.ts
+++ src/api/user/auth/localAuthApi.ts
@@ -75,7 +75,14 @@ export async function requestEmailAuthCode(c: Context): Promise<Response> {
     const emailSent = await sendAuthCodeByEmail(c, email, authCode);
     if (!emailSent) {
         await prisma.$disconnect();
-        return c.json({ error: "인증 코드 이메일 전송에 실패했습니다." }, 500);
+        return c.json(
+          {
+            error: "인증 코드 이메일 전송에 실패했습니다.",
+            emailSent: emailSent,
+            MAIL_SENDER: c.env.EMAIL_SENDER,
+          },
+          500
+        );
     }
 
     await prisma.$disconnect();
